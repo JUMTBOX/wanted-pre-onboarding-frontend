85291870432dc6faa5dbd2d0b665383fa6288788
JUMTBOX <yjey12@naver.com>
2023-08-10 12:26:02 +0900

    ...

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Main() {
-  return <div className="bg-gray-800 text-white">Main</div>;
+  return (
+    <>
+      <div className="bg-gray-800 text-white">Main</div>
+    </>
+  );
 }
